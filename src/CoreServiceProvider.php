67471a8f028a27d05742a60e0452e5e9ce24e582
kid <kidiatoliny@akira-io.com>
2026-01-18 19:35:31 +0100

    feat: register EnsureModuleActive middleware and ModuleRegistry in CoreServiceProvider

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Hunter\Core;
 
 use Hunter\Core\Commands\CoreCommand;
+use Hunter\Core\Http\Middleware\EnsureModuleActive;
+use Hunter\Core\Module\ModuleRegistry;
+use Illuminate\Routing\Router;
+use Override;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -12,16 +16,33 @@ final class CoreServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('hunter-core')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_hunter_core_table')
             ->hasCommand(CoreCommand::class);
+    }
+
+    #[Override]
+    public function packageRegistered(): void
+    {
+        $this->app->singleton('hunter.modules', static fn (): ModuleRegistry => new ModuleRegistry());
+
+        $this->app->alias('hunter.modules', ModuleRegistry::class);
+    }
+
+    #[Override]
+    public function packageBooted(): void
+    {
+        $this->registerMiddlewareAlias();
+    }
+
+    private function registerMiddlewareAlias(): void
+    {
+        /** @var Router $router */
+        $router = $this->app->make(Router::class);
+
+        $router->aliasMiddleware('module.active', EnsureModuleActive::class);
     }
 }
